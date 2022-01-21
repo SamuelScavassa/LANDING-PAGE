837fc7c8c62a9e54096f3998df20f3a0fa5ac02e
SamuelScavassa <samuel040103@gmail.com>
2022-01-21 16:14:50 -0300

    commit

---
+++ Exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello wolrd, my first program");
+		System.out.println("Teste ");
+	}	
+}
